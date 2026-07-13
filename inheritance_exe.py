761384b9a9ea9106e484dc9b3ad1e37e6d48cefb
chaim <chchdeu@gmail.com>
2026-07-13 14:21:39 +0300

    mission_4

--- inheritance_exe.py
+++ inheritance_exe.py
@@ -41,3 +41,29 @@ class Cyclist(Athleth):
 rider1=Cyclist("chaim",22,"trek")
 rider1.introduce()
 rider1.discribe_gear()
+
+#mission 4
+class Athleth:
+    def __init__(self,name,country):
+        self.name=name
+        self.country=country
+    def greet(self):
+        print(f"{self.name} represent {self.country}")
+class Swimmer(Athleth):
+    def __init__(self, name, country,stroke_style):
+        super().__init__(name, country)
+        self.stroke_style=stroke_style
+class Runner(Athleth):
+    def __init__(self, name, country,best_distance):
+        super().__init__(name, country)
+        self.best_distance=best_distance
+class Cyclist(Athleth):
+    def __init__(self, name, country,race_type):
+        super().__init__(name, country)
+        self.race_type=race_type
+lior=Swimmer("lior","israel","freestyle")
+avi=Runner("avi","kenya","maratohn")
+jan=Cyclist("jan","france","road")
+lior.greet()
+avi.greet()
+jan.greet()
