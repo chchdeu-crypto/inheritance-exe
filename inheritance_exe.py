3b691dbd57fb6603fbad2965e798db2866a186c0
chaim <chchdeu@gmail.com>
2026-07-13 13:44:54 +0300

    mission_2

--- inheritance_exe.py
+++ inheritance_exe.py
@@ -10,3 +10,17 @@ class Swimmer(Athleth):
         super().__init__(name, age)
 tom=Swimmer("tom",22)
 tom.introduce()
+
+#mission 2
+class Athleth:
+    def __init__(self,name,age,sport):
+        self.name=name
+        self.age=age
+        self.sport=sport
+    def describe(self):
+        print(f"{self.name} competes in {self.sport}")
+class Runner(Athleth):
+    def __init__(self, name, age):
+        super().__init__(name, age, "running")
+sara=Runner("sara",25)
+sara.describe()
